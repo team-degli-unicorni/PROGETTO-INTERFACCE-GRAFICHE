8d299a308204b8c264f174369ae95ddea12fc279
giorgio rinchi <giorgio.rinchi@gmail.com>
2019-09-16 14:05:18 +0200

    prova

--- AwayFromTheMilkyWay/src/awayfromthemilkyway/view/Universe.java
+++ AwayFromTheMilkyWay/src/awayfromthemilkyway/view/Universe.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package awayfromthemilkyway.view;
-
+//BALDI PUZZA
 
 import awayfromthemilkyway.utils.Configuration;
 import awayfromthemilkyway.utils.Resources;
